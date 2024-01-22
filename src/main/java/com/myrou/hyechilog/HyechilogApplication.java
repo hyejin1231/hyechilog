f6993e1481f71b806a46f2b53f33d767185a36eb
hyejin <phj981231@naver.com>
2024-01-22 22:30:42 +0900

    2024.01.22. Initial commit

---
+++ src/main/java/com/myrou/hyechilog/HyechilogApplication.java
@@ -0,0 +1,13 @@
+package com.myrou.hyechilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyechilogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyechilogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myrou/hyechilog/HyechilogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myrou.hyechilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyechilogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
