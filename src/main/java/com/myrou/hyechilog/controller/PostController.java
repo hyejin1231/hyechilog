39cc7b3e36263a4918b5db31fe6980a24879ca88
hyejin <phj981231@naver.com>
2024-01-23 21:52:02 +0900

    [2024.01.23] PostController get, post method 작성 및 테스트 + mediaType

--- src/main/java/com/myrou/hyechilog/controller/PostController.java
+++ src/main/java/com/myrou/hyechilog/controller/PostController.java
@@ -1,8 +1,12 @@
 package com.myrou.hyechilog.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.myrou.hyechilog.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@Slf4j
 @RestController
 public class PostController {
 
@@ -12,8 +16,27 @@ public class PostController {
             // -> javascript + <-> api (json)
     // vue, nuxt (vue + SSR)
     // react, next (react + SSR)
-    @GetMapping("/v1/posts")
+
+    // HTTP Method => GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
+    // 이 특징을 다 알아야 한다!!
+    @GetMapping("/v1/gets")
     public String get() {
         return "Hello world";
     }
+
+    @PostMapping("/v1/posts")
+//    public String post(@RequestParam("title") String title, @RequestParam("content") String content) { // 방법 1
+//    public String post(@RequestParam Map<String, String> params) { // 방법2
+    public String post(@ModelAttribute PostCreate params) { // 방법2
+//        log.info("title={}, content={}", title, content);
+        log.info("params={}", params);
+        return "Hello Post World";
+    }
+
+    @PostMapping("/v1/posts2")
+    public String post2(@RequestBody PostCreate params) { // 방법2
+        log.info("params={}", params);
+        return "Hello Post World";
+    }
+
 }
