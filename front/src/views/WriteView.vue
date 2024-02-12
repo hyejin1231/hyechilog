dc2b88c1407ed0cd0c83973102f6d60c359752f6
hyejin <phj981231@naver.com>
2024-02-12 14:13:07 +0900

    [2024.02.12] Vue.js3 을 이용해 초간단 글 작성 화면 만들기

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  import {ref} from "vue";
+  import axios from "axios";
+
+  const title = ref("")
+  const content = ref("")
+
+  const write = function () {
+    console.log(title.value + " / " + content.value)
+    axios.post("http://localhost:8080/api/v1/blogs/new", {
+      title:title.value, content: content.value
+    })
+  };
+
+
+</script>
+
+<template>
+  <div class="mt-2">
+    <el-input type="text" placeholder="제목을 입력해주세요." v-model="title"/>
+  </div>
+
+  <div>
+    <div class="mt-2">
+    <el-input type="textarea" rows="15" v-model="content"></el-input>
+    </div>
+
+    <div class="mt-2">
+      <el-button @click="write()">등록 완료!</el-button>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
