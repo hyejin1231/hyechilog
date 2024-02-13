133364c0282e474a642a810b6d685f1de498446c
hyejin <phj981231@naver.com>
2024-02-13 22:29:05 +0900

    [2024.02.13] 글 리스트 화면 만들기

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
   import {ref} from "vue";
   import axios from "axios";
+  import {useRouter} from "vue-router";
 
   const title = ref("")
   const content = ref("")
+
+  const  router = useRouter()
 
   const write = function () {
     console.log(title.value + " / " + content.value)
     axios.post("/my-backend/api/v1/blogs/new", {
       title:title.value, content: content.value
+    }).then(() => {
+      router.replace({name : "home"})
     })
   };
 
