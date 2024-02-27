fccf56d301ff11730a5f539888befda9bf16e1a5
hyejin1231 <phj981231@naver.com>
2024-02-27 10:41:45 +0900

    [2024.02.27] Spring Security formLogin 설정 추가

--- src/main/java/com/myrou/hyechilog/config/SecurityConfig.java
+++ src/main/java/com/myrou/hyechilog/config/SecurityConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -32,7 +38,32 @@ public class SecurityConfig
 								authorize.requestMatchers("/auth/login").permitAll()
 										.anyRequest().authenticated()
 				)
+				.formLogin(
+						form ->
+								form.loginPage("/auth/login")
+										.loginProcessingUrl("/auth/login")
+										.usernameParameter("username")
+										.passwordParameter("password")
+										.defaultSuccessUrl("/")
+				)
 				.csrf(AbstractHttpConfigurer::disable)
 				.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService()
+	{
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		UserDetails user = User.withUsername("hyechi").password("1231")
+				.roles("ADMIN").build();
+		manager.createUser(user);
+		
+		return manager;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
