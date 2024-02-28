8a7854a4e108305580c7691e5b940c2d61c6934b
hyejin1231 <phj981231@naver.com>
2024-02-28 10:42:39 +0900

    [2024.02.28] Spring Security 사용자 정보 조회

--- src/main/java/com/myrou/hyechilog/api/controller/MainController.java
+++ src/main/java/com/myrou/hyechilog/api/controller/MainController.java
@@ -1,7 +1,10 @@
 package com.myrou.hyechilog.api.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.myrou.hyechilog.config.security.UserPrincipal;
 
 /**
  * 사용자 권한에 따른 페이지 접속 확인을 위한 테스트 페이지
@@ -11,7 +14,7 @@ public class MainController
 {
 	
 	@GetMapping("/user")
-	public String user()
+	public String user(@AuthenticationPrincipal UserPrincipal userPrincipal)
 	{
 		return "사용자 페이지 입니다. ❤";
 	}
